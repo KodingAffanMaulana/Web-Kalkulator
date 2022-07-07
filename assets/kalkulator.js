52a93e12b4d045ecfb039c62b465642f883cb437
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2022-07-07 17:42:48 +0700

    finish him

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -94,5 +94,15 @@ function performCalculation(){
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
 
-    calculator.displayNumber = result;   
+    //objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+
+    putHistory(history);
+    calculator.displayNumber = result;
+    renderHistory();
 }
